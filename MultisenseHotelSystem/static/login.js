5d2bfe9aa221f0117cea8683004e986b34678a4e
钩钩么么哒 <a516550257@qq.com>
2015-11-25 22:20:59 +0800

    增加注册页面

--- MultisenseHotelSystem/static/login.js
+++ MultisenseHotelSystem/static/login.js
@@ -1,7 +1,7 @@
 var Logo = React.createClass({
   render: function() {
     return (
-      <p className = "logo">Multisense Hotel System</p>
+      <p className = "logo">Multisense Hotel System {this.props.func} View</p>
     )
   }
 })
@@ -10,7 +10,8 @@ var LoginView = React.createClass({
   getInitialState: function(){
     return {
       username: "",
-      password: ""
+      password: "",
+      loginOrReg: true
     };
   },
   handleSubmit: function(){
@@ -23,27 +24,83 @@ var LoginView = React.createClass({
       }
     })
   },
+  registerRequest: function(){
+    this.setState({
+      loginOrReg: false
+    })
+  },  
+
+  backToLogin:function(){
+    this.setState({
+      loginOrReg: true
+    })
+  },
+  updateUsername:function(ev){
+    this.setState({
+      username: ev.target.value
+    })
+  },
+  updatePassword:function(ev){
+    this.setState({
+      password: ev.target.value
+    })
+  },
   render: function() {
-    return (
-      <div className = "login">
-        <Logo/>
-        <form className = "form-horizontal loginForm">
-          <div className = "form-group">
-            <label htmlFor="username" className="col-sm-2 col-md-2 col-lg-2 control-label">Username</label>
-            <div className = "col-sm-10 col-md-10 col-lg-10">
-              <input type = "text" className = "form-control" id = "username" placeholder="Username" />
+    if (this.state.loginOrReg){
+      return (
+        <div className = "login">
+          <Logo
+            func = {"Login"}
+          ></Logo>
+          <form className = "form-horizontal loginForm">
+            <div className = "form-group">
+              <label htmlFor="username" className="col-sm-2 col-md-2 col-lg-2 control-label">Username</label>
+              <div className = "col-sm-10 col-md-10 col-lg-10">
+                <input type = "text" className = "form-control" id = "username" placeholder="Username" value={this.state.username} onChange={this.updateUsername}/>
+              </div>
             </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password" className="col-sm-2 col-md-2 col-lg-2 control-label">Password</label>
-            <div className = "col-sm-10 col-md-10 col-lg-10">
-              <input type="password" className="form-control" id="password" placeholder="Password" />
+            <div className="form-group">
+              <label htmlFor="password" className="col-sm-2 col-md-2 col-lg-2 control-label">Password</label>
+              <div className = "col-sm-10 col-md-10 col-lg-10">
+                <input type="password" className="form-control" id="password" placeholder="Password" value={this.state.password} onChange={this.updatePassword}/>
+              </div>
             </div>
-          </div>
-          <button type="button" onClick={this.handleSubmit} className="btn btn-default loginButton">Login</button>
-        </form>
-      </div>
-    );
+            <button type="button" onClick={this.handleSubmit} className="btn btn-default loginButton">Login</button>
+            <button type="button" onClick={this.registerRequest} className="btn btn-default registerButton">Register</button>
+          </form>
+        </div>
+      );
+    }else{
+      return (
+        <div className = "register">
+          <Logo
+            func = {"Register"}
+          ></Logo>
+          <form className = "form-horizontal registerForm">
+            <div className = "form-group">
+              <label htmlFor="username" className="col-sm-2 col-md-2 col-lg-2 control-label">Username</label>
+              <div className = "col-sm-10 col-md-10 col-lg-10">
+                <input type = "text" className = "form-control" id = "username" placeholder="Username" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="col-sm-2 col-md-2 col-lg-2 control-label">Password</label>
+              <div className = "col-sm-10 col-md-10 col-lg-10">
+                <input type="password" className="form-control" id = "password" placeholder="Password" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email" className="col-sm-2 col-md-2 col-lg-2 control-label">Email</label>
+              <div className = "col-sm-10 col-md-10 col-lg-10">
+                <input type="email" className="form-control" id = "password" placeholder="email" />
+              </div>
+            </div>
+            <button type="button" onClick={this.handleSubmit} className="btn btn-default loginButton">Login</button>
+            <button type="button" onClick={this.backToLogin} className="btn btn-default backButton">Back</button>
+          </form>
+        </div>
+      )
+    }
   }
 });
 
